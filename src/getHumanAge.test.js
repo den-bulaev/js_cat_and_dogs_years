fe29ed10f14ba117583c6603fc1ac2be24ad31eb
den-bulaev <denisbulaev7@gmail.com>
2022-08-06 18:15:51 +0300

    tests were finished

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,25 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('array length should be equal to 2', () => {
+    const expectedResult = getHumanAge(15, 15);
+
+    expect(expectedResult)
+      .toHaveLength(2);
+  });
+
+  test('should be zero if lower than 1 human year', () => {
+    const expectedResult = getHumanAge(14, 14);
+
+    expect(expectedResult)
+      .toEqual([0, 0]);
+  });
+
+  test('cat and dog converted years are not equal from 28 animal years', () => {
+    const expectedResult = getHumanAge(28, 28);
+
+    expect(expectedResult)
+      .toEqual([3, 2]);
+  });
 });
